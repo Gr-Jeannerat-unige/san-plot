a9f117808fa5b707318ca77aa2aed7be34d7358c
Damien Jeannerat <damien.jeannerat@unige.ch>
2019-02-22 08:51:26 +0100

    update demo matlab file

--- very_short_demo.m
+++ very_short_demo.m
@@ -1,5 +1,5 @@
 %% setting and reading spectrum 
-file_id=fopen('/Volumes/lacie_case/nmr_data/nmrge500_3/data/winssinger/data/nmr/nmr/DC15-40/10/pdata/1/1r');
+file_id=fopen('./dj-caryophyllene_oxide/12/pdata/4/1r');
 spectrum=fread(file_id,'int');
 %% plotting spectrum for test and reference
 figure(1);clf;plot(spectrum)
@@ -16,3 +16,6 @@ Sminus=sort(Sminus,'descend');
 figure(2);clf;
 loglog(Splus);hold on
 %loglog(Sminus)
+Smax=Splus(1,1);
+SigmaN=Splus(round(size(Splus,1)/2),1)/0.6744897495;
+title(['Smax=' num2str(Smax) ', SigmaN=' num2str(SigmaN), ', SINO=' num2str(Smax/(2*SigmaN))])
